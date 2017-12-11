3e5bc0b1d064d3afbfb5f5d786f60a7349c073a1
OrdenKrieger <OrdenKriegerEDR@googlemail.com>
2017-12-11 19:07:30 +0100

    Rebrand to Project New World (Pnw)
    Signed-off-by: OrdenKrieger <OrdenKriegerEDR@googlemail.com>

--- android/variable.go
+++ android/variable.go
@@ -20,7 +20,7 @@ import (
 	"runtime"
 	"strings"
 
-	"lineage/soong/android"
+	"pnw/soong/android"
 
 	"github.com/google/blueprint/proptools"
 )
@@ -88,8 +88,8 @@ type variableProperties struct {
 			Cppflags []string
 		}
 
-		// include Lineage variables
-		Lineage android.Product_variables
+		// include Pnw variables
+		Pnw android.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -156,8 +156,8 @@ type productVariables struct {
 
 	Override_rs_driver *string `json:",omitempty"`
 
-	// include Lineage variables
-	Lineage android.ProductVariables
+	// include Pnw variables
+	Pnw android.ProductVariables
 }
 
 func boolPtr(v bool) *bool {
